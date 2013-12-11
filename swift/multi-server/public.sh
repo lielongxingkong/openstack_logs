dadcabed6c586e906dfb0bd4f5ce601871968c64
lielongxingkong <zzl_1164@126.com>
2013-12-11 12:56:23 +0800

    ip & hostname support in public

--- swift/multi-server/public.sh
+++ swift/multi-server/public.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 cp ./hosts /etc/hosts
+export LOCAL_NAME=datanode01
+export LOCAL_IP=$LOCAL_NAME
+echo $LOCAL_NAME > /etc/hostname
+hostname $LOCAL_NAME
+
 apt-get update 
 PROJECT=swift
 #ZIP=swift.zip
 #ZIPNAME=swift
-CODE=git://github.com/openstack/swift.git 
+CODE=http://121.199.49.51/openstack/swift.git 
 
 #unzip $ZIP 
 #cp -r  ./$PROJECT /opt/$PROJECT
@@ -18,10 +23,11 @@ git clone $CODE /opt/$PROJECT
 cd /opt/$PROJECT
 git fetch && git checkout stable/havana
 pip install -r /opt/$PROJECT/requirements.txt
-python setup.py install
+#python setup.py install
 cd -
 
-mkdir -p /var/cache/swift /srv/node/
+mkdir -p /var/cache/swift
+find /var/cache/swift* -type f -name *.recon -exec rm -f {} \;
 mkdir -p /etc/swift/
 
 cat >/etc/swift/swift.conf <<EOF
@@ -29,8 +35,11 @@ cat >/etc/swift/swift.conf <<EOF
 swift_hash_path_suffix = randomestringchangeme
 EOF
 
+rm -f /var/log/debug /var/log/messages /var/log/rsyncd.log /var/log/syslog
+
 apt-get install -y rsyslog
 mkdir -p /var/log/swift
+find /var/log/swift -type f -exec rm -f {} \;
 chown -R syslog.adm /var/log/swift
 cat >/etc/rsyslog.d/10-swift.conf<<EOF
 local1,local2,local3,local4.*   /var/log/swift/all.log
@@ -51,3 +60,4 @@ local4.*;local4.!notice        /var/log/swift/proxy.log
 local4.notice                /var/log/swift/proxy.error
 local4.*
 EOF
+service rsyslog restart
