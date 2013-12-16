56938d18afac47384106eef930f1c049dba5c56a
lielongxingkong <zzl_1164@126.com>
2013-12-17 00:47:07 +0800

    add new function to fab

--- fabfile.py
+++ fabfile.py
@@ -17,6 +17,28 @@ env.roledefs = {
 
 env.password = "root"
 
+DATA_DISK = 'vdc'
+META_DISKS = ['vdc', 'vdd', 'vde', 'vdf']
+
+@roles('data')
+def data_mount():
+    run('mount -t xfs /dev/vdc /srv/node/vdc')
+    run('mount')
+    run('ls /srv/node/vdc')
+
+@roles('meta')
+def meta_mount():
+    for disk in META_DISKS:
+	run('mount -t xfs /dev/%s /srv/node/%s' % (disk, disk))
+	run('ls /srv/node/%s' % disk)
+    run('mount')
+
+@roles('data', 'meta', 'proxy')
+def cleanlog():
+    run('service rsyslog stop')
+    run('rm /var/log/swift/*')
+    run('service rsyslog start')
+
 @roles('data')
 def resetdata():
     with cd('/root/windchimes'):
@@ -35,7 +57,7 @@ def update():
     with cd('/opt/windchimes'):
         run('git checkout develop')
         run('git pull')
-        run('python setup.py install')
+        run('python setup.py develop')
 
 @roles('data')
 def data_restart():
@@ -43,17 +65,41 @@ def data_restart():
 
 @roles('meta')
 def meta_restart():
-    run('swift-init all restart')
+    run('swift-init account restart')
+    run('swift-init container restart')
+    run('swift-init object restart')
 
 @roles('proxy')
 def proxy_restart():
     run('swift-init proxy restart')
 
-@roles('data', 'meta', 'proxy')
-def cleanlog():
-    run('service rsyslog stop')
-    run('rm /var/log/swift/*')
-    run('service rsyslog start')
+@roles('data')
+def data_start():
+    run('swift-init storage start')
+
+@roles('meta')
+def meta_start():
+    run('swift-init account start')
+    run('swift-init container start')
+    run('swift-init object start')
+
+@roles('proxy')
+def proxy_start():
+    run('swift-init proxy start')
+
+@roles('proxy')
+def proxy_stop():
+    run('swift-init proxy stop')
+
+@roles('data')
+def data_stop():
+    run('swift-init storage stop')
+
+@roles('meta')
+def meta_stop():
+    run('swift-init account stop')
+    run('swift-init container stop')
+    run('swift-init object stop')
 
 def dotask():
     execute(cleanlog)
@@ -68,3 +114,13 @@ def restart():
     execute(data_restart)
     execute(meta_restart)
     execute(proxy_restart)
+
+def start():
+    execute(data_start)
+    execute(meta_start)
+    execute(proxy_start)
+
+def stop():
+    execute(data_stop)
+    execute(meta_stop)
+    execute(proxy_stop)
